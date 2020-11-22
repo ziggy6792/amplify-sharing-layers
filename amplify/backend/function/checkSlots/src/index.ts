b360274b7ff5964cfef342188697dab39f5f8431
Simon <simonaverhoeven@gmail.com>
2020-11-23 01:15:41 +0800

    my first commit

---
+++ amplify/backend/function/checkSlots/src/index.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+// import MyLib from '/opt/nodejs/build/MyLib';
+import MyLib from '/Users/sive/Documents/SWT/wake-book/amplify/backend/function/wakebookLayer/lib/nodejs/build/MyLib';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const handler = async (event, context, callback) => {
+  try {
+    const result = MyLib();
+    console.log('result', result);
+  } catch (err) {
+    console.log({ err });
+    callback(err, null);
+  }
+  return null;
+};
